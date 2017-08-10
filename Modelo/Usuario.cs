d5663a6e5bd2f644afb2ff0e472228ba2c365557
Marco <atpires34@yahoo.com.br>
2017-08-10 17:11:03 -0300

    asd

--- Modelo/Usuario.cs
+++ Modelo/Usuario.cs
@@ -7,7 +7,7 @@ namespace Modelo
 {
     public class Usuario
     {
-        public int Id { get; set; }
+        public int UsuarioId { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Email { get; set; }
