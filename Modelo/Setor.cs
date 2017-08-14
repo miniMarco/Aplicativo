c8dbb961b2835895ae1a6cec4fad9ba66a958167
Marco <atpires34@yahoo.com.br>
2017-08-14 16:51:57 -0300

    app

--- Modelo/Setor.cs
+++ Modelo/Setor.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Modelo
 {
     public class Setor
     {
+        [DisplayName("Setor")]
         public int SetorId { get; set; }
         public string Nome { get; set; }
 
--- Modelo/Usuario.cs
+++ Modelo/Usuario.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Modelo
 {
@@ -7,7 +8,11 @@ namespace Modelo
         public int UsuarioId { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [DataType(DataType.Password)]
         public string Senha { get; set; }
         public Setor Setor { get; set; }
         public char Sexo { get; set; }
